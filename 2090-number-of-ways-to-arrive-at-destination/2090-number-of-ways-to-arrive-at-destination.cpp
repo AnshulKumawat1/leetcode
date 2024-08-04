f6ed99d6d37b00985835e5d0f7a826b5c97c675e
Anshul kumawat <157249189+AnshulKumawat1@users.noreply.github.com>
2024-08-04 21:55:41 +0530

    Time: 63 ms (31.92%), Space: 30.7 MB (99.95%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    int mod = 1e9 + 7;
+
+public:
+    int countPaths(int n, vector<vector<int>> &roads)
+    {
+        vector<vector<pair<int, int>>> adj(n);
+        for (int i = 0; i < roads.size(); i++)
+        {
+            // {u, v, w}
+            adj[roads[i][0]].push_back({roads[i][1], roads[i][2]});
+            adj[roads[i][1]].push_back({roads[i][0], roads[i][2]});
+        }
+        vector<long long> weight(n, 1e15);
+        vector<int> way(n, 0);
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        pq.push({0, 0});
+        weight[0] = 0;
+        way[0] = 1;
+        while (!pq.empty())
+        {
+            auto it = pq.top();
+            pq.pop();
+
+            long long dist = it.first;
+            int node = it.second;
+
+            if (node == n - 1)
+            {
+                break;
+            }
+            for (auto &&neighbour : adj[node])
+            {
+                int newnode = neighbour.first;
+                long long newdist = neighbour.second;
+
+                if (newdist + dist < weight[newnode])
+                {
+                    weight[newnode] = newdist + dist;
+                    way[newnode] = way[node];
+                    pq.push({newdist + dist, newnode});
+                }
+                else if (newdist + dist == weight[newnode])
+                {
+                    way[newnode] = (way[newnode] + way[node] * 1LL) % mod;
+                }
+            }
+        }
+        return way[n - 1];
+    }
+};
